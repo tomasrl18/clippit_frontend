ba2cbdf15b8db9a27c8e480f5afa67f0746a51cc
Tomás R. <tomaslopezraigal@gmail.com>
2025-02-04 12:34:38 +0100

    Login and Sign Up details

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -70,15 +70,14 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 90vh;
-  background-color: #f4f4f9;
+  height: 70vh;
 }
 
 .login-box {
   background-color: #ffffff;
   padding: 50px 70px;
   border-radius: 12px;
-  box-shadow: 0 8px 12px rgba(0, 0, 0, 0.1);
+  box-shadow: 10px 10px 15px 10px rgba(0, 0, 0, 0.5);
   width: 100%;
   max-width: 500px;
   text-align: center;
